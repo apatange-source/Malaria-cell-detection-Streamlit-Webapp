91b3efeadb5a41318760e80616cc2d2219dbc9a4
apatange-source <83966389+apatange-source@users.noreply.github.com>
2021-05-11 18:13:25 +0530

    Update img_preprocess.py

--- models/img_preprocess.py
+++ models/img_preprocess.py
@@ -6,22 +6,22 @@ from sklearn.utils import shuffle
 
 data = []
 labels = []
-Parasitized = os.listdir("C:/Users/aishw/PycharmProjects/pythonProject/cell_images/Parasitized/")
+Parasitized = os.listdir("cell_images/Parasitized/")
 for parasite in Parasitized:
-    image = cv2.imread("C:/Users/aishw/PycharmProjects/pythonProject/cell_images/Parasitized/" + parasite)
-    print("C:/Users/aishw/PycharmProjects/pythonProject/cell_images/Parasitized/" + parasite)
+    image = cv2.imread("cell_images/Parasitized/" + parasite)
+    print("cell_images/Parasitized/" + parasite)
     image_from_array = Image.fromarray(image, 'RGB')
-    size_image = image_from_array.resize((50, 50))
+    size_image = image_from_array.resize((150, 150))
     data.append(np.array(size_image))
     labels.append(0)
 
 
-Uninfected = os.listdir("C:/Users/aishw/PycharmProjects/pythonProject/cell_images/Uninfected/")
+Uninfected = os.listdir("pythonProject/cell_images/Uninfected/")
 for uninfect in Uninfected:
-    image = cv2.imread("C:/Users/aishw/PycharmProjects/pythonProject/cell_images/Uninfected/" + uninfect)
-    print("C:/Users/aishw/PycharmProjects/pythonProject/cell_images/Uninfected/" + uninfect)
+    image = cv2.imread("cell_images/Uninfected/" + uninfect)
+    print("cell_images/Uninfected/" + uninfect)
     image_from_array = Image.fromarray(image, 'RGB')
-    size_image = image_from_array.resize((50, 50))
+    size_image = image_from_array.resize((150, 150))
     data.append(np.array(size_image))
     labels.append(1)
 
@@ -33,7 +33,7 @@ labels = np.array(labels)
 (X_train, X_val, X_test) = df[int(0.2 * len(df)):], df[int(0.1 * len(df)):int(0.2 * len(df))], df[:int(0.1 * len(df))]
 (y_train, y_val, y_test) = labels[int(0.2 * len(labels)):], labels[int(0.1 * len(labels)):int(0.2 * len(labels))], labels[:int(0.1 * len(labels))]
 
-np.savez("C:/Users/aishw/PycharmProjects/pythonProject/cell_images/training_arrays.npz", X_train, X_val, y_val, y_train, X_test, y_test)
+np.savez("cell_images/training_arrays.npz", X_train, X_val, y_val, y_train, X_test, y_test)
 
 
 
